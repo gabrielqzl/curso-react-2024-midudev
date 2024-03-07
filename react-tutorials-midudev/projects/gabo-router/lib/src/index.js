739bcac7d0e9340af4bc2a8321f3c7e0ba308f02
Gabriel Quiroz <gabrielquirozdev@gmail.com>
2024-03-07 15:38:57 -0400

    Update

---
+++ react-tutorials-midudev/projects/gabo-router/lib/src/index.js
@@ -0,0 +1,1 @@
+export{Router}from"./components/Router.jsx";export{Link}from"./components/Link.jsx";export{Route}from"./components/Route.jsx";
---
+++ react-tutorials-midudev/projects/gabo-router/src/index.jsx
@@ -0,0 +1,3 @@
+export { Router } from './components/Router.jsx'
+export { Link } from './components/Link.jsx'
+export { Route } from './components/Route.jsx'
